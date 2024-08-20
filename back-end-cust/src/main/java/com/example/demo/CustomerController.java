ccb9ef38f217e2b005b751756e60d1e425f1f3cd
hattvr <hatterhg@gmail.com>
2024-08-20 10:53:34 -0400

    update: customer object + endpoint mapping

--- back-end-cust/src/main/java/com/example/demo/CustomerController.java
+++ back-end-cust/src/main/java/com/example/demo/CustomerController.java
@@ -1,10 +1,25 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
+    List<Customer> customers = new ArrayList<Customer>();
+
+    public CustomerController() {
+        Customer customer1 = new Customer("John Doe", "john@gmail.com", "user", 1);
+        Customer customer2 = new Customer("Jane Doe", "jane@gmail.com", "user", 2);
+        Customer customer3 = new Customer("Zaeem", "zaeem@gmail.com", "admin", 3);
+
+        customers.add(customer1);
+        customers.add(customer2);
+        customers.add(customer3);
+    }
 
     @GetMapping("/")
     public String checkConnection() {
@@ -13,11 +28,23 @@ public class CustomerController {
 
     @GetMapping("/customers")
     public String getAllCustomers() {
-        return "Here's your beautiful list of all the customers!";
+        String allCustomers = "";
+
+        for (Customer customer : customers) {
+            allCustomers += customer.toString() + "\n";
+        }
+
+        return allCustomers;
     }
 
     @GetMapping("/customers/{id}")
-    public String getCustomerById() {
-        return "Here's the customer with the id you requested!";
+    public String getCustomerById(@PathVariable int id) {
+        for (Customer customer : customers) {
+            if (customer.getId() == id) {
+                return customer.toString();
+            }
+        }
+
+        return "Customer not found!";
     }
 }
