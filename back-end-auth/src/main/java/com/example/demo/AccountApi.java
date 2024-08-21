fe8c58a70d055e6eaeb50562c3a4cb051e5e2f2f
Junove <nathaniel.endick@gmail.com>
2024-08-21 14:37:02 +0000

    SpringBoot project setup + dependencies for jwt

---
+++ back-end-auth/src/main/java/com/example/demo/AccountApi.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApi.class, args);
+	}
+
+}
