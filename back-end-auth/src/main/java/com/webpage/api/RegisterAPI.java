21b114da4b6ce998d1643180d0ca78bbe10fd10c
Xiyu Wan <xiyuwanbjtu@gmail.com>
2024-08-22 19:20:30 +0000

    register v0

---
+++ back-end-auth/src/main/java/com/webpage/api/RegisterAPI.java
@@ -0,0 +1,72 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.MalformedInputException;
+
+import javax.tools.DocumentationTool;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.webpage.domain.CustomerEntity;
+
+@RestController
+@RequestMapping("/register")
+public class RegisterAPI {
+
+    @PostMapping
+    public ResponseEntity<?> registerCustomer(@RequestBody CustomerEntity newCustomer, UriComponentsBuilder uri){
+        if (newCustomer.getId() != 0 || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        postCustomer(newCustomer);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newCustomer.getId()).toUri();
+        ResponseEntity<?> response = ResponseEntity.created(location).build();
+        return response;
+    }
+    
+    private void postCustomer(CustomerEntity newCustomer){
+        try {
+            URL url = new URL("http://localhost:8080/api/customers");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = conn.getOutputStream();
+            os.write(newCustomer.toString().getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                throw new RuntimeException("Failed. HTTP Response Code: " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String output;
+            System.out.println("Output from server ... \n");
+            while ((output = br.readLine()) !=  null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+        } catch (MalformedInputException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ back-end-auth/src/main/java/com/webpage/domain/CustomerEntity.java
@@ -0,0 +1,61 @@
+package com.webpage.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="CUSTOMERS")
+public class CustomerEntity {
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
